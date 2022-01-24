fc166bffcfc02955a50b1939d16becab77328831
michaelaldas <vmaldas@pucese.edu.ec>
2022-01-23 22:41:43 -0500

    Agregar archivos de proyecto.

---
+++ Aldas/Migrations/202201240325364_CreatePost.cs
@@ -0,0 +1,28 @@
+﻿namespace Aldas.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreatePost : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Posts",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        DatePublised = c.DateTime(nullable: false),
+                        Title = c.String(),
+                        Body = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Posts");
+        }
+    }
+}
---
+++ Aldas/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace Aldas.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Aldas.BlogDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Aldas.BlogDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
